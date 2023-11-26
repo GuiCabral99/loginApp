c8f3bf7aa577e77b5f06d70439dac3ae98b6b620
Guilherme Cabral <gui.cabral7641@gmail.com>
2023-11-26 08:32:49 -0300

    temporary removal of the export const authOptions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import SignInForm from "@/components/SignInForm";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+// import { getServerSession } from "next-auth";
+// import { redirect } from "next/navigation";
+// import { authOptions } from "./api/auth/[...nextauth]/rou:te";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
 
-  if (session) redirect("/dashboard");
+  // if (session) redirect("/dashboard");
   return (
     <main>
       <SignInForm />
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,12 +1,12 @@
 import SignUpForm from "@/components/SignUpForm";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+// import { getServerSession } from "next-auth";
+// import { redirect } from "next/navigation";
+// import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export default async function signup() {
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
 
-  if (session) redirect("/dashboard");
+  // if (session) redirect("/dashboard");
 
   return (
     <main className="flex flex-col items-center justify-center h-screen">
